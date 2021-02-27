2ec7f07ca9a924db480f0c90d6831ca512fd914f
Eustacedn <stanislavle@gmail.com>
2021-02-27 16:56:12 +0300

    Слайды

---
+++ js/slider.js
@@ -0,0 +1,19 @@
+var btn = document.querySelectorAll(".sliderswitch");
+var openedTab = function(evt){
+  var blocks = document.querySelectorAll(".slider-container > div")
+  for(var j = 0; j < blocks.length; j++){
+      blocks[j].classList.remove("slide-current")
+    if(evt.currentTarget.id === "btn1"){
+      blocks[0].classList.add("slide-current");
+    }
+    if(evt.currentTarget.id === "btn2"){
+      blocks[1].classList.add("slide-current");
+    }
+    if(evt.currentTarget.id === "btn3"){
+      blocks[2].classList.add("slide-current");
+    }
+  }
+}
+for(var i = 0; i < btn.length; i++){
+  btn[i].addEventListener("click", openedTab)
+}
